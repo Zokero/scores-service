12582d5843ee2c61ddd8074d5d99b8ab74041848
Zokero <piotrekkukiela@gmail.com>
2022-09-04 19:25:03 +0200

    First commit

---
+++ src/test/java/com/pkuk/scores/ScoresServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkuk.scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoresServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
