a7f9ae3d0e0a8e75a99b64e5bcd07381a19dd69f
Zokero <piotrekkukiela@gmail.com>
2022-09-09 05:29:02 +0200

    enable scheduling

---
+++ src/main/java/com/pkuk/scores/task/Urls.java
@@ -0,0 +1,10 @@
+package com.pkuk.scores.task;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Urls {
+
+    public static final String LZPN_URL = "https://lzpn.org/terminarze-i-tabele/#48506";
+}
